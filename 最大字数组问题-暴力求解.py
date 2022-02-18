c7993e1b174e7c3b6e9f6729568b4d581e90f2b5
LuckyMth <35443522+LuckyMth@users.noreply.github.com>
2022-02-18 14:52:52 +0800

    11

--- 最大字数组问题-暴力求解.py
+++ 最大字数组问题-暴力求解.py
@@ -2,22 +2,22 @@
 import random
 
 
-def get_ram_array():
+def get_ram_array(_num):
     _ram = []
     k = random.randint(0, 1000)
-    for _i in range(100):
+    for _i in range(_num):
         while k in _ram:
-            k = random.randint(0, 1000)
+            k = random.randint(0, 500)
         _ram.append(k)
     return _ram
 
 
 # 最大子数组问题：给定数组，在前端选择较小值，在后端选择较大值，作差。输出最大差值及选择的方法
 if __name__ == "__main__":
-    ram = get_ram_array()
+    ram = get_ram_array(50)
     print(ram)
     # 暴力求解
-    mi= -1
+    mi = -1
     ii = -1
     jj = -1
     for i in range(len(ram)-1):
